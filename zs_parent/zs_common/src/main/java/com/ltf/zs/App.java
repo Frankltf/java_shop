a7b70e104c6208b5459e7bcfbd6706e6a8092648
liutengfei <langerfei@163.com>
2018-09-19 14:05:07 +0800

    one

---
+++ zs_parent/zs_common/src/main/java/com/ltf/zs/App.java
@@ -0,0 +1,13 @@
+package com.ltf.zs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
